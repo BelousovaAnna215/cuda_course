19ab4824096d338701d470dc18348e1a0ce1a9a7
BelousovaAnna215 <113383599+BelousovaAnna215@users.noreply.github.com>
2024-12-05 17:57:32 +0300

    Update jacobi.cu

--- task1/jacobi.cu
+++ task1/jacobi.cu
@@ -1,95 +1,56 @@
 /* Jacobi-3 program */
 
 #include <cuda_runtime.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>  
-#include <time.h>
 #include <thrust/device_vector.h>
-#include <thrust/transform.h>
 #include <thrust/functional.h>
-#include <thrust/reduce.h>
+#include <thrust/transform.h>
+#include <chrono>
+#include <cstdio>
+#include <fstream>
 #include <iostream>
-#include <cstring>
-
-#define SAFE_CALL(call) do { \
-    cudaError_t err = call; \
-    if (err != cudaSuccess) { \
-        printf("Error: %s at %s:%d\n", cudaGetErrorString(err), __FILE__, __LINE__); \
-        exit(1); \
-    } \
-} while (0)
+using namespace std;
 
 #define Max(a, b) ((a) > (b) ? (a) : (b))
-
-#define A(i,j,k) A[((i)*L+(j))*L+(k)]
-#define B(i,j,k) B[((i)*L+(j))*L+(k)]
-#define C(i,j,k) C[((i)*L+(j))*L+(k)]
-
-#define L 887
+#define ind(i, j, k) (((i)*L + (j))*L + (k))
+#define L 384
 #define ITMAX 100
 
 double eps;
 double MAXEPS = 0.5;
 
-__global__ void initialKernel(double *A, double *B) {
-    int i = blockIdx.z * blockDim.z + threadIdx.z;
-    int j = blockIdx.y * blockDim.y + threadIdx.y;
-    int k = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (i < L && j < L && k < L) {
-        A(i, j, k) = 0;
-        if (i == 0 || j == 0 || k == 0 || i == L - 1 || j == L - 1 || k == L - 1) {
-            B(i, j, k) = 0;
-        } else {
-            B(i, j, k) = 4 + i + j + k;
-        }
-    }
-}
-
-__global__ void differenceKernel(double *A, const double *B, double* C) {
-    int i = blockIdx.z * blockDim.z + threadIdx.z;
-    int j = blockIdx.y * blockDim.y + threadIdx.y;
-    int k = blockIdx.x * blockDim.x + threadIdx.x;
-	
-    if (i > 0 && i < L - 1)
-	if (j > 0 && j < L - 1)
-	    if (k > 0 && k < L - 1) {
-                C(i,j,k) = fabs(B(i,j,k) - A(i,j,k));
-                A(i,j,k) = B(i,j,k);
-	    }
-}
-
 __global__ void functionKernel(const double *A, double *B) {
     int i = blockIdx.z * blockDim.z + threadIdx.z;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
     int k = blockIdx.x * blockDim.x + threadIdx.x;
+
     if (i > 0 && i < L - 1)
-	if (j > 0 && j < L - 1)
-	    if (k > 0 && k < L - 1) {
-		B(i,j,k) = (A(i - 1,j,k) + A(i,j - 1,k) + A(i,j,k - 1) + A(i,j,k + 1) + A(i,j + 1,k) + A(i + 1,j,k)) / 6.0f;
-	    }
+        if (j > 0 && j < L - 1)
+            if (k > 0 && k < L - 1) {
+                B[ind(i, j, k)] =
+                    (A[ind(i - 1, j, k)] + A[ind(i, j - 1, k)] + A[ind(i, j, k - 1)] +
+                     A[ind(i, j, k + 1)] + A[ind(i, j + 1, k)] + A[ind(i + 1, j, k)]) / 6.0;
+            }
 }
 
 double compare_arrays(double *cpuArray, double *gpuArray) {
     double max_diff = 0;
-    for (int i = 0; i < L*L*L; ++i) {
-	double tmp = fabs(cpuArray[i] - gpuArray[i]);
-	max_diff = Max(tmp, max_diff);
+    for (int i = 0; i < L * L * L; ++i) {
+        double tmp = fabs(cpuArray[i] - gpuArray[i]);
+        max_diff = Max(tmp, max_diff);
     }
     return max_diff;
 }
 
 void initial(double *A, double *B) {
     for (int i = 0; i < L; ++i)
-	for (int j = 0; j < L; ++j)
-	    for (int k = 0; k < L; ++k) {
-		A(i,j,k) = 0;
-		if (i == 0 || j == 0 || k == 0 || i == L - 1 || j == L - 1 || k == L - 1)
-		    B(i,j,k) = 0;
-		else
-		    B(i,j,k) = 4 + i + j + k;
-	    }
+        for (int j = 0; j < L; ++j)
+            for (int k = 0; k < L; ++k) {
+                A[ind(i, j, k)] = 0;
+                if (i == 0 || j == 0 || k == 0 || i == L - 1 || j == L - 1 || k == L - 1)
+                    B[ind(i, j, k)] = 0;
+                else
+                    B[ind(i, j, k)] = 4 + i + j + k;
+            }
 }
 
 void print3DArray(double *A) {
@@ -97,7 +58,7 @@ void print3DArray(double *A) {
         std::cout << "Layer i = " << i << ":\n";
         for (int j = 0; j < L; ++j) {
             for (int k = 0; k < L; ++k) {
-                std::cout << A(i,j,k) << " ";
+                std::cout << A[ind(i, j, k)] << " ";
             }
             std::cout << "\n";
         }
@@ -111,120 +72,122 @@ int main(int an, char **as) {
     bool cpu_gpu = false;
 
     if (an > 1) {
-	if (strcmp(as[1], "c") == 0) {
-	    cpu = true;
-	} else if (strcmp(as[1], "g") == 0) {
-	    gpu = true;
-	} else if (strcmp(as[1], "cg") == 0) {
-	    cpu_gpu = true;
-	}
+        if (strcmp(as[1], "c") == 0) {
+            cpu = true;
+        } else if (strcmp(as[1], "g") == 0) {
+            gpu = true;
+        } else if (strcmp(as[1], "cg") == 0) {
+            cpu_gpu = true;
+        }
     }
-    
+
     float time_taken_cpu = 0;
-    
-    size_t size = L * L * L * sizeof(double);
-    
-    double *A = (double*)malloc(size);
-    double *B = (double*)malloc(size);
+    float time_taken_gpu = 0;
+
+    size_t size = L * L * L;
+
+    double *A = (double *)malloc(size * sizeof(double));
+    double *B = (double *)malloc(size * sizeof(double));
 
     if (cpu || cpu_gpu) {
-	initial(A, B);
-	clock_t start_time = clock();
+        initial(A, B);
 
-	for (int it = 1; it <= ITMAX; it++) {
-	    eps = 0;
-				
-	    for (int i = 1; i < L - 1; i++)
-	        for (int j = 1; j < L - 1; j++)
-		    for (int k = 1; k < L - 1; k++) {
-			double tmp = fabs(B(i,j,k) - A(i,j,k));
-			eps = Max(tmp, eps);
-			A(i,j,k) = B(i,j,k);
-		    }
-				
-	    for (int i = 1; i < L - 1; i++)
-		for (int j = 1; j < L - 1; j++)
-		    for (int k = 1; k < L - 1; k++)
-			B(i,j,k) = (A(i - 1,j,k) + A(i,j - 1,k) + A(i,j,k - 1) + A(i,j,k + 1) + A(i,j + 1,k) + A(i + 1,j,k)) / 6.0f;
-				
-	    // printf(" IT = %4i   EPS = %14.7E\n", it, eps);
-	    if (eps < MAXEPS)
-		break;
+        clock_t start_time = clock();
+
+        for (int it = 1; it <= ITMAX; it++) {
+            eps = 0;
+
+            for (int i = 1; i < L - 1; i++)
+                for (int j = 1; j < L - 1; j++)
+                    for (int k = 1; k < L - 1; k++) {
+                        double tmp = fabs(B[ind(i, j, k)] - A[ind(i, j, k)]);
+                        eps = Max(tmp, eps);
+                        A[ind(i, j, k)] = B[ind(i, j, k)];
+                    }
+
+            for (int i = 1; i < L - 1; i++)
+                for (int j = 1; j < L - 1; j++)
+                    for (int k = 1; k < L - 1; k++) {
+                        B[ind(i, j, k)] =
+                            (A[ind(i - 1, j, k)] + A[ind(i, j - 1, k)] + A[ind(i, j, k - 1)] +
+                             A[ind(i, j, k + 1)] + A[ind(i, j + 1, k)] + A[ind(i + 1, j, k)]) /
+                            6.0;
+                    }
+
+            if (eps < MAXEPS)
+                break;
         }
-			
-	clock_t end_time = clock();
-	time_taken_cpu = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
 
-	printf(" CPU Completed.\n");
-	printf(" CPU time = %12.2lf\n", time_taken_cpu);		
+        clock_t end_time = clock();
+        time_taken_cpu = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+
+        printf(" CPU Completed.\n");
+        printf(" CPU time = %12.2lf\n", time_taken_cpu);
     }
 
-    float time_taken_gpu = 0;
-    double *gA = (double*)malloc(size);
-    
-    if (gpu || cpu_gpu) {
-	double *d_A, *d_B;
-	SAFE_CALL(cudaMalloc((void **)&d_A, size));
-	SAFE_CALL(cudaMalloc((void **)&d_B, size));
-		
-	dim3 thread(16, 8, 8);
-	dim3 block((L + thread.x - 1) / thread.x, 
-		   (L + thread.y - 1) / thread.y, 
-		   (L + thread.z - 1) / thread.z);
-		
-	initialKernel<<<block, thread>>>(d_A, d_B);
-	SAFE_CALL(cudaGetLastError());	
+    thrust::host_vector<double> h_A(size), h_B(size);
+    thrust::device_vector<double> d_A(size), d_B(size), diff_A_B(size);
 
-	cudaEvent_t start_time, end_time;
+    if (gpu || cpu_gpu) {
+        initial(h_A.data(), h_B.data());
+
+        thrust::copy(h_A.begin(), h_A.end(), d_A.begin());
+        thrust::copy(h_B.begin(), h_B.end(), d_B.begin());
+
+        dim3 thread(32, 4, 4);
+        dim3 block((L + thread.x - 1) / thread.x,
+                   (L + thread.y - 1) / thread.y,
+                   (L + thread.z - 1) / thread.z);
+
+        cudaEvent_t start_time, end_time;
         cudaEventCreate(&start_time);
         cudaEventCreate(&end_time);
-		
-	cudaEventRecord(start_time, 0);
-		
-	for (int it = 1; it <= ITMAX; it++) {
-	    thrust::device_vector<double> diff(L*L*L);
-	    double *ptrdiff = thrust::raw_pointer_cast(&diff[0]);
-	    differenceKernel<<<block, thread>>>(d_A, d_B, ptrdiff);
-	    SAFE_CALL(cudaGetLastError());
-			
-	    double eps = thrust::reduce(diff.begin(), diff.end(), 0.0, thrust::maximum<double>());
-			
-	    functionKernel<<<block, thread>>>(d_A, d_B);
-	    SAFE_CALL(cudaGetLastError());
-		   
-	    // printf(" IT = %4i   EPS = %14.7E\n", it, eps);
-	    if (eps < MAXEPS)
-		break;
-	}
-		
-	cudaEventRecord(end_time, 0);
-	cudaEventSynchronize(end_time);
+        cudaEventRecord(start_time, 0);
+
+        for (int it = 1; it <= ITMAX; it++) {
+            thrust::transform(d_A.begin(), d_A.end(), d_B.begin(), diff_A_B.begin(),
+                              thrust::minus<double>());
+
+            double eps = thrust::transform_reduce(
+                diff_A_B.begin(), diff_A_B.end(),
+                [] __device__(double x) { return x < 0.0 ? -x : x; }, 0.0,
+                thrust::maximum<double>());
+
+            double *A_ptr = thrust::raw_pointer_cast(d_A.data());
+            double *B_ptr = thrust::raw_pointer_cast(d_B.data());
+
+            if (it % 2 == 1)
+                functionKernel<<<block, thread>>>(B_ptr, A_ptr);
+            else
+                functionKernel<<<block, thread>>>(A_ptr, B_ptr);
+
+            cudaStreamSynchronize(0);
+
+            if (eps < MAXEPS)
+                break;
+        }
+
+        cudaEventRecord(end_time, 0);
+        cudaEventSynchronize(end_time);
         cudaEventElapsedTime(&time_taken_gpu, start_time, end_time);
         cudaEventDestroy(start_time);
         cudaEventDestroy(end_time);
-		
-	time_taken_gpu = time_taken_gpu * 0.001;
-		
-	SAFE_CALL(cudaMemcpy(gA, d_A, size, cudaMemcpyDeviceToHost));
 
-        SAFE_CALL(cudaFree(d_A));
-        SAFE_CALL(cudaFree(d_B));
+        time_taken_gpu = time_taken_gpu * 0.001;
 
-	printf(" GPU Completed.\n");
-	printf(" Time in seconds = %12.2lf\n", time_taken_gpu);
+        h_A = d_A;
+        h_B = d_B;
+
+        printf(" GPU Completed.\n");
+        printf(" Time in seconds = %12.2lf\n", time_taken_gpu);
     }
 
     if (cpu_gpu) {
-        std::cout << "acceleration = " << time_taken_cpu / time_taken_gpu <<  std::endl;
-        
-        // print3DArray(A);
-        // print3DArray(gA);
-        std::cout << "max difference = " << compare_arrays(A, gA) << std::endl;
-        // if max difference == 0 - same results
+        std::cout << "acceleration = " << time_taken_cpu / time_taken_gpu << std::endl;
+        std::cout << "max difference = " << compare_arrays(A, h_A.data()) << std::endl;
     }
-    
+
     free(A);
-    free(gA);
     free(B);
 
     return 0;
